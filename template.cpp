83c82505a7a61aeb4835df4361e4697ff907b131
Urachan1203 <keisuke.pingpongwin8@gmail.com>
2019-11-17 10:47:14 +0900

    ver1.0.0

---
+++ template.cpp
@@ -0,0 +1,15 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int) (n); i++)
+typedef long long ll;
+
+
+int main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+
+    return 0;
+}
