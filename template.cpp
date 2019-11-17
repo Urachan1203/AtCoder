6c5c2e8a9432b53cc5f4e6fb898fe37cec47fd57
Urachan1203 <keisuke.pingpongwin8@gmail.com>
2019-11-17 14:20:37 +0900

    ver1.1.0

--- template.cpp
+++ template.cpp
@@ -2,14 +2,23 @@
 using namespace std;
 
 #define rep(i, n) for (int i = 0; i < (int) (n); i++)
+#define Rrep(i,n) for(int i=((int)n-1); i>=0; i--)
+#define all(x) (x).begin(),(x).end()
+#define SZ(x) ((int)(x).size())
+#define INF 2e15
+#define MOD 10e9+7
+
+typedef vector<int> vint;
 typedef long long ll;
+
 
 
 int main()
 {
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-    cout << fixed << setprecision(15);
+        cin.tie(nullptr);
+        ios_base::sync_with_stdio(false);
+        cout << fixed << setprecision(15);
 
-    return 0;
+
+        return 0;
 }
